51c374e9fca23d8629ce24347ecfbe2a920c81a3
Jeremy Swinnen <jswinnen@me.com>
2019-11-28 15:17:25 +0100

    Adding Slack

--- setup.sh
+++ setup.sh
@@ -24,6 +24,7 @@ brew cask install imageoptim
 brew cask install istat-menus
 brew cask install iterm2
 brew cask install sip
+brew cask install slack
 brew cask install spectacle
 brew cask install spotify
 brew cask install vanilla
