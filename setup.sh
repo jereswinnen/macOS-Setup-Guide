e373bd7f2bbc8baabe5019ffb581c894b80c75b7
Jeremy Swinnen <jswinnen@me.com>
2019-11-28 15:15:38 +0100

    Removing unused apps

--- setup.sh
+++ setup.sh
@@ -14,30 +14,20 @@ brew cask install qlimagesize
 
 # Install Apps
 brew cask install 1password
-brew cask install abstract
 brew cask install bettertouchtool
 brew cask install codekit
-brew cask install cyberduck
 brew cask install figma
 brew cask install github
 brew cask install google-chrome
-brew cask install highsierramediakeyenabler
 brew cask install imagealpha
 brew cask install imageoptim
 brew cask install istat-menus
 brew cask install iterm2
-brew cask install selfcontrol
 brew cask install sip
-brew cask install sketch
 brew cask install spectacle
 brew cask install spotify
-brew cask install subler
-brew cask install timing
-brew cask install transmission
-brew cask install transmit
 brew cask install vanilla
 brew cask install visual-studio-code
-brew cask install vlc
 
 # Install Homebrew MAS
 brew install mas
